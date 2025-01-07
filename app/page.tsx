c5bad4992d5a3d8037a7ee587ec11c3d463bc81e
rtznaga <aaronvinhle@gmail.com>
2025-01-07 13:52:18 -0800

    logos and json updates for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Earnings from "@/components/Earnings";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>hi</h1>
+    <div className="flex min-h-screen flex-col items-center">
+      <main className="flex flex-col items-center p-24 flex-grow">
+        <h1 className="text-2xl font-bold mb-4">earnings.fyi</h1>
+        <p className="text-lg text-center mb-4">
+          This is not financial advice. Read the disclaimer.
+        </p>
+        <Earnings />
       </main>
       <Footer />
     </div>
