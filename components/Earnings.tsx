59f050524fabfa791a170c8a95dd670bc22687f0
rtznaga <aaronvinhle@gmail.com>
2025-01-08 14:08:10 -0800

    remove supabase utils

--- components/Earnings.tsx
+++ components/Earnings.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import isoWeek from 'dayjs/plugin/isoWeek';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
