f87c11dd60c19608bbbde8aec498ddb909bce8b1
rtznaga <aaronvinhle@gmail.com>
2025-01-08 20:36:18 -0800

    create card component

---
+++ components/Earnings.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Earnings() {
+    return(
+        <Card>
+            <CardHeader>
+                <CardTitle>this is a card title</CardTitle>
+                card header 2
+            </CardHeader>
+            <CardContent>
+                <p>this is card content</p>
+            </CardContent>
+        </Card>
+    )
+}
