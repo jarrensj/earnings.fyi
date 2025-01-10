5c0bf1e27468d42ea929fb41929cde3fc67b6dda
rtznaga <aaronvinhle@gmail.com>
2025-01-10 10:05:07 -0800

    update ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-G-L2C4W4DJ93');
+              gtag('config', 'G-L2C4W4DJ93');
             `}
           </Script>
         </head>
