4f29b5014f86769f10fc5b30fc61ab027a0470ee
Jarren San Jose <sanjose.jarren@gmail.com>
2025-01-11 12:46:14 -0800

    address hydration error on the time display

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,30 @@
 'use client'
 
+import { useEffect, useState } from 'react';
 import Earnings from "@/components/Earnings";
 import Footer from "@/components/Footer";
 
 export default function Home() {
+  const [dateTime, setDateTime] = useState('');
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      const now = new Date();
+      setDateTime(`${now.toLocaleDateString('en-US', { dateStyle: 'full' })}, ${now.toLocaleTimeString('en-US', { timeStyle: 'long' })}`);
+    };
+
+    updateDateTime();
+    const intervalId = setInterval(updateDateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
         <h1 className="text-2xl font-bold">earnings.fyi</h1>
         <p className="text-center text-xs text-gray-500">
-          {`${new Date().toLocaleDateString('en-US', { dateStyle: 'full' })}, ${new Date().toLocaleTimeString('en-US', { timeStyle: 'long' })}`}
+          {dateTime}
         </p>
         <Earnings />
       </main>
