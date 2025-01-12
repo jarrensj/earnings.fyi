e7cabf84b43526d394e6f911d95da8345709d47b
Jarren San Jose <sanjose.jarren@gmail.com>
2025-01-11 16:33:09 -0800

    edit disclaimer on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer () {
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <p>Disclaimer: This is not financial advice.</p>
+      <p>
+        Disclaimer: This is not financial advice. We may own positions in whatever that&apos;s mentioned. You are responsible for your own decisions and your own losses. Information may not be accurate. This is not a complete calendar.
+      </p>
     </footer>
   );
 }
