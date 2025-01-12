f37068868398100e0ee064014368f6a56d0a1fb4
Jarren San Jose <sanjose.jarren@gmail.com>
2025-01-12 00:52:16 -0800

    add dev credit on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
+
 export default function Footer () {
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       <p>
         Disclaimer: This is not financial advice. We may own positions in whatever that&apos;s mentioned. You are responsible for your own decisions and your own losses. Information may not be accurate. This is not a complete calendar.
+      </p>
+      <p className="text-sm">
+        code by&nbsp;<Link href="https://discord.gg/omakase" className="text-blue-400 hover:text-blue-600"target="_blank" rel="noopener noreferrer">omakase</Link>
       </p>
     </footer>
   );
