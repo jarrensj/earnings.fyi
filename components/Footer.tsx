e4df4740813f29d3150b0a27cb1a0343d58ec097
rtznaga <aaronvinhle@gmail.com>
2025-01-12 20:21:12 -0800

    logo discalimer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer () {
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       <p>
-        Disclaimer: This is not financial advice. We may own positions in whatever that&apos;s mentioned. You are responsible for your own decisions and your own losses. Information may not be accurate. This is not a complete calendar.
+        Disclaimer: The information provided on this website is for informational purposes only. This is not financial advice. We may own positions in whatever that&apos;s mentioned. You are responsible for your own decisions and your own losses. Information may not be accurate. This is not a complete calendar. All company logos and trademarks displayed on this website are the property of their respective owners and do not imply any affiliation with earnings.fyi.
       </p>
       <p className="text-sm">
         code by&nbsp;<Link href="https://discord.gg/omakase" className="text-blue-400 hover:text-blue-600"target="_blank" rel="noopener noreferrer">omakase</Link>
