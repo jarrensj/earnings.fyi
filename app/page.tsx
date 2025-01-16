72c30ecb104525778cc22bd1e32f8564f859ff2d
rtznaga <aaronvinhle@gmail.com>
2025-01-16 13:38:47 -0800

    clean comments

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,6 @@ import Link from 'next/link'
 const orbitron = Orbitron({ subsets: ['latin'] })
 
 
-/* export default function Home() {
-  return (
-    <div className="grid grid-rows-[auto_1fr_auto] items-start justify-items-center min-h-screen p-4 pb-16 gap-8 sm:p-16 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
-        <h1 className="text-2xl font-bold">glasscannon.co</h1>
-        <Footer />
-      </main>
-    </div>
-  );
-} */ 
-
   export default function Home() {
     return (
       <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-4 sm:p-8 overflow-hidden">
